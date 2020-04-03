654af449ae8043f10281658b607f0cd1f0d4b0e5
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-03 12:57:22 +0200

    Update ClientController.java

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -1,1 +1,33 @@
 
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Article;
+
+import com.example.demo.service.ClientService;
+
+@RestController
+public class ClientController {
+
+	@Autowired
+	private ClientService service;
+	
+	@GetMapping("/article")
+	public ResponseEntity<List<Article>> getAllProducts(){
+		return service.findAllArticle();
+	}
+	
+	@GetMapping("/article/{id}")
+	public Optional<Article> getProductById(@PathVariable("id") String id ){
+		return service.getProductById(id);
+	}
+	
+}
