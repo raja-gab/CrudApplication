a35f57695c170ec637b2e95bb563b77ca1d3e4d1
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-03 13:09:23 +0200

    Update ArticleController.java

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,1 +1,37 @@
 
+package com.example.demo.controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.repository.ArticleRepository;
+
+import lombok.var;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+@RestController
+public class ArticleController {
+	
+	
+	@Autowired
+	ArticleRepository articleRepo;
+	
+	@RequestMapping(value = "/sid/{imageName}", method = RequestMethod.GET,
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> getImage(@PathVariable("imageName") String imageName) throws IOException {
+
+        var imgFile = new ClassPathResource("static/images/"+imageName+".jpg");
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(new InputStreamResource(imgFile.getInputStream()));
+    }
+}
