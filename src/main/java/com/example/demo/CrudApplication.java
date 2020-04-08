c52389236548d1fdbbcb3bfe5cd9baf28329b224
raja-gab <52567610+raja-gab@users.noreply.github.com>
2020-04-08 16:46:34 +0100

    last commit

--- src/main/java/com/example/demo/CrudApplication.java
+++ src/main/java/com/example/demo/CrudApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient 
 public class CrudApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
