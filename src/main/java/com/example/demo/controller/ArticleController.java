dc4b990c901f92a83e53d842a2e83d250fa2f125
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-09 00:11:16 +0200

    Update ArticleController.java

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,33 +1,38 @@
 package com.example.demo.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.example.demo.entity.Article;
+import com.example.demo.entity.Client;
+import com.example.demo.entity.Fournisseur;
 import com.example.demo.repository.ArticleRepository;
+import com.example.demo.repository.ClientRepository;
+import com.example.demo.repository.FournisseurRepository;
 
 import lombok.var;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
 @RestController
 public class ArticleController {
 	
-	
+	@Autowired
+	private FournisseurRepository fourRepo;
 	@Autowired
 	ArticleRepository articleRepo;
+	@Autowired
+	private ClientRepository clientRepo;
+	
 	
 	@RequestMapping(value = "/sid/{imageName}", method = RequestMethod.GET,
             produces = MediaType.IMAGE_JPEG_VALUE)
@@ -40,4 +45,32 @@ public class ArticleController {
                 .contentType(MediaType.IMAGE_JPEG)
                 .body(new InputStreamResource(imgFile.getInputStream()));
     }
+	@GetMapping("/getallarticle")
+	public List<Article> getAll(){
+		return articleRepo.findAll();
+	}
+	
+	
+	@GetMapping("/getproduct/{id}")
+	public Optional<Article> getProdcutById(@PathVariable("id") String id ){
+		return articleRepo.findById(id);
+	}
+	
+	@GetMapping("/getallfour")
+	public List<Fournisseur> getAllFour(){
+		return fourRepo.findAll();
+	}
+	@GetMapping("/getallclient")
+	public List<Client> getAllClient(){
+		return clientRepo.findAll();
+	}
+	
+	
+
 }
+
+
+
+
+
+
