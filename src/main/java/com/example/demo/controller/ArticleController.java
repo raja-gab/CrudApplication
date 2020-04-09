a4eacbde0cfe09174e9515aec8b82f44e9f87a2f
raja-gab <52567610+raja-gab@users.noreply.github.com>
2020-04-09 21:29:35 +0100

    Update ArticleController.java

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,23 +1,19 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.entity.Article;
-import com.example.demo.entity.Client;
-import com.example.demo.entity.Fournisseur;
+
 import com.example.demo.repository.ArticleRepository;
-import com.example.demo.repository.ClientRepository;
-import com.example.demo.repository.FournisseurRepository;
+
 
 import lombok.var;
 import org.springframework.http.MediaType;
@@ -25,14 +21,10 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 public class ArticleController {
-	
-	@Autowired
-	private FournisseurRepository fourRepo;
+
 	@Autowired
 	ArticleRepository articleRepo;
-	@Autowired
-	private ClientRepository clientRepo;
-	
+
 	
 	@RequestMapping(value = "/sid/{imageName}", method = RequestMethod.GET,
             produces = MediaType.IMAGE_JPEG_VALUE)
