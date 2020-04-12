aaa4ca666c6cb89bf1852ed1e5d64d4b7fb6517f
raja-gab <52567610+raja-gab@users.noreply.github.com>
2020-04-12 17:06:40 +0100

    commit controller

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,18 +1,24 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
-
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Article;
+import com.example.demo.entity.Client;
+import com.example.demo.entity.Fournisseur;
 import com.example.demo.repository.ArticleRepository;
+import com.example.demo.repository.ClientRepository;
+import com.example.demo.repository.FournisseurRepository;
 
 
 import lombok.var;
@@ -25,19 +31,43 @@ public class ArticleController {
 	@Autowired
 	ArticleRepository articleRepo;
 
+	@Autowired 
+	ClientRepository clientRepository ;
+	
+	@Autowired
+	FournisseurRepository fournisseurRepository ;
 	
 	@RequestMapping(value = "/sid/{imageName}", method = RequestMethod.GET,
             produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<InputStreamResource> getImage(@PathVariable("imageName") String imageName) throws IOException {
 
         var imgFile = new ClassPathResource("static/images/"+imageName+".jpg");
-
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.IMAGE_JPEG)
                 .body(new InputStreamResource(imgFile.getInputStream()));
     }
-
+	
+	@GetMapping("/getallarticle")
+	public List<Article> getAll(){
+		return articleRepo.findAll();
+	}
+	
+	
+	@GetMapping("/getproduct/{id}")
+	public Optional<Article> getProdcutById(@PathVariable("id") String id ){
+		return articleRepo.findById(id);
+	}
+	
+	@GetMapping("/getallfour")
+	public List<Fournisseur> getAllFour(){
+		return fournisseurRepository.findAll();
+	}
+	@GetMapping("/getallclient")
+	public List<Client> getAllClient(){
+		return clientRepository.findAll();
+	}
+   
 	
 	
 
--- src/main/java/com/example/demo/repository/FournisseurRepository.java
+++ src/main/java/com/example/demo/repository/FournisseurRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.demo.entity.Fournisseur;
 @RepositoryRestResource(collectionResourceRel = "Fournisseurs", path = "fournisseur")
 public interface FournisseurRepository extends MongoRepository<Fournisseur,String>{
+	
+	
+
+	Optional<Fournisseur> findByIdFour(String idFour);
 
 }
